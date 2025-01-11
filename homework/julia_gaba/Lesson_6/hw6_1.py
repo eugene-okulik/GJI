d8f2d48ea13539641b63e7ec329aeb805cae8094
Julia Gaba <gabamarket87@gmail.com>
2025-01-12 02:42:33 +0300

    GJI Lesson 6 Done

---
+++ homework/julia_gaba/Lesson_6/hw6_1.py
@@ -0,0 +1,11 @@
+phrase = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+words = phrase.split()
+
+print(words)
+
+for word in words:
+    new_words = []
+    word_ing = word.strip(',.') + 'ing'
+    new_words.append(word_ing)
+
+    print(new_words)
