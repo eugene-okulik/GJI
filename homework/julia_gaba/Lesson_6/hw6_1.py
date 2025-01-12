0244a32b4b89894de8d580732e9c6fb3e251606e
Julia Gaba <gabamarket87@gmail.com>
2025-01-12 18:17:49 +0300

    GJI Update 2 Lesson 6

--- homework/julia_gaba/Lesson_6/hw6_1.py
+++ homework/julia_gaba/Lesson_6/hw6_1.py
@@ -1,15 +1,24 @@
 
-phrase = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
-          "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
+text = ("Etiam tincidunt neque erat, quis molestie enim imperdiet vel."
+        "Integer urna nisl, facilisis vitae semper at, dignissim vitae libero")
 
-words = phrase.split()
+print(type(text))
+words = text.split()
 
 print(words)
 
+new_text = []
 for word in words:
-    new_words = []
-    word_ing = word.strip(',.') + 'ing'
-    new_words.append(word_ing)
-    new_phrase = ' '.join(new_words)
 
-    print(new_phrase, end=' ')
+    word_ing = word.strip(',.') + 'ing'
+
+    if word.endswith(','):
+        word_ing += ','
+    elif word.endswith('.'):
+        word_ing += '.'
+
+    new_text.append(word_ing)
+
+new_text = ' '.join(new_text)
+
+print(new_text)
