5322dbe671e78401ed511055ce17af27c2aa6272
Julia Gaba <gabamarket87@gmail.com>
2025-01-15 12:51:39 +0300

    GJI Update 2 Lesson 8

--- homework/julia_gaba/Lesson_8/8-2.py
+++ homework/julia_gaba/Lesson_8/8-2.py
@@ -1,39 +1,26 @@
-def fibonacci(limit=110_000):
+import sys
+sys.set_int_max_str_digits(100000)
+
+
+def fibonacci():
     f1 = 0
     f2 = 1
-    count = 1
-    while count < limit:
-        yield f2
-        f2 += f1
-        count += 1
+    while True:
+        yield f1
+        old_f1, old_f2 = f1, f2
+        f1 = old_f1 + old_f2
+        f2 = f1
 
 
+count_list = [5, 200, 1000, 100_000]
+
+results = []
 count = 1
-for number in fibonacci(110_000):
-    if count == 5:
-        print(number)
-        break
-    count += 1
 
-count = 1
-for number in fibonacci(110_000):
-    if count == 200:
+for number in fibonacci():
+    if count in count_list:
+        results.append(number)
         print(number)
-        break
+        if len(results) == len(count_list):
+            break
     count += 1
-
-count = 1
-for number in fibonacci(110_000):
-    if count == 1000:
-        print(number)
-        break
-    count += 1
-
-count = 1
-for number in fibonacci(110_000):
-    if count == 100_000:
-        print(number)
-        break
-    count += 1
-
-# я что-то напутала, помогите плиз!
