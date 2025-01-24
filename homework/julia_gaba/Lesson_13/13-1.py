cb45067f2e1b9fa9a825d48b5ddfb8e4ad13d86c
Julia Gaba <gabamarket87@gmail.com>
2025-01-24 14:09:32 +0300

    GJI deleted white space

--- homework/julia_gaba/Lesson_13/13-1.py
+++ homework/julia_gaba/Lesson_13/13-1.py
@@ -8,8 +8,7 @@ homework_path = os.path.dirname(os.path.dirname(base_path))
 print(homework_path)
 
 eugene_file_path = os.path.join(
-    homework_path, 'eugene_okulik', 'hw_13', 'data.txt'
-    )
+    homework_path, 'eugene_okulik', 'hw_13', 'data.txt')
 print(eugene_file_path)
 
 
@@ -28,7 +27,6 @@ with open('hw13-1.txt', 'w') as julis_file:
         print(str(date_str))
 
         date = datetime.datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S.%f")
-       
         if "на неделю позже" in data_line:
             new_date = date + datetime.timedelta(weeks=1)
             formatted_date = (
@@ -39,12 +37,10 @@ with open('hw13-1.txt', 'w') as julis_file:
             julis_file.write(formatted_date + "\n")
             formatted_date_str = str(formatted_date)
             print(formatted_date_str)
-            
         if "день недели" in data_line:
             weekday = date.strftime('%A')
             julis_file.write(weekday + "\n")
             print(weekday)
-
         if "сколько дней назад" in data_line:
             days_ago = (datetime.datetime.now() - date).days
             julis_file.write(str(days_ago) + "\n")
